d37bacc6a0ff7be86b1d6ef05c6cdbd76bace84e
drmuzik <drmusicpiano@gmail.com>
2023-08-31 16:22:11 +0100

    initial commit: created nestjs and added GraphQl resource for users

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
